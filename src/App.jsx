b9ae5a3facf9e269788553df5160eefed611efc8
Mohammed Farhan Islam <farhanislam670@gmail.com>
2024-01-12 21:49:36 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from "react";
+import "./App.css";
+import AudioRecorder from "./components/AudioRecorder";
+
+function App() {
+  return (
+    <div>
+      <h1>React Audio Recorder</h1>
+      <div>
+        <AudioRecorder />
+      </div>
+    </div>
+  );
+}
+
+export default App;
